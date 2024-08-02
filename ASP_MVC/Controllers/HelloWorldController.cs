8aee743f6dc9e059a4920e2a0674661f95a53ce4
TopMaster98765 <topmaster98765@gmail.com>
2024-08-02 17:41:01 +0900

    Add Movies Page(Controller, Model, View, Migration, SeedData)

---
+++ ASP_MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace ASP_MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ ASP_MVC/Data/ASP_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP_MVC.Models;
+
+namespace ASP_MVC.Data
+{
+    public class ASP_MVCContext : DbContext
+    {
+        public ASP_MVCContext (DbContextOptions<ASP_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP_MVC.Models.Movie> Movie { get; set; }
+    }
+}
