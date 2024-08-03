96a90b62122af4aafb88c338f74fd25ab1975840
TopMaster98765 <topmaster98765@gmail.com>
2024-08-03 21:57:24 +0900

    filtering by movie name

--- ASP_MVC/Models/Movie.cs
+++ ASP_MVC/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASP_MVC.Models
 {
@@ -10,9 +11,13 @@ namespace ASP_MVC.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
