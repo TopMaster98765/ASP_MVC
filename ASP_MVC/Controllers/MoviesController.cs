dc52c60a7e297822d703ad251808d748f97e18e7
TopMaster98765 <topmaster98765@gmail.com>
2024-08-05 01:09:41 +0900

    filtering by movie genre

--- ASP_MVC/Controllers/MoviesController.cs
+++ ASP_MVC/Controllers/MoviesController.cs
@@ -20,16 +20,34 @@ namespace ASP_MVC.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            var movies = from m in _context.Movie select m;
-
-            if (!String.IsNullOrEmpty(searchString))
+            if (_context.Movie == null)
             {
-                movies = movies.Where(s => s.Title.Contains(searchString));
+                return Problem("Entity set 'MvcMovieContext.Movie' is null.");
             }
 
-            return View(await movies.ToListAsync());
+            // Use LING to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            var movies = from m in _context.Movie select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
